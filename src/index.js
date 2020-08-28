30da4ac308444d24825d03f731991dd689aa76be
Shaylin Chetty <shaylinc13@gmail.com>
2020-08-28 21:57:20 +0200

    Added audio testing service for both http and https sources

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ app.use(express.json());
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "/client/view"));
 
-// const createAudioSourceModel = require("./data/audioSource/createAudioSourceModel");
-// const createAudioSourceRoutes = require("./api/audioSource/createAudioSourceRoutes");
 const createRecordingTaskModel = require("./data/recordingTask/createRecordingTaskModel");
+const AudioSourceTester = require("./logic/audioSource/audioSourceTester");
 
 function main() {
+	let audioSourceTester = new AudioSourceTester();
+	audioSourceTester.testAudioSource({url: "https://playerservices.streamtheworld.com/api/livestream-redirect/987FM_PREM.aac"}).then((result) => console.log(result));
+
 	initApplication().then(() => console.log("Application initialised"));
 }
 
@@ -19,9 +21,7 @@ async function initApplication() {
 	
 	let recordingTasks = await recordingTaskModel.getRecordingTasks();
 	console.log(recordingTasks);
-
-	// createAudioSourceRoutes(app, audioSourceModel);
-
+	
 	app.get("/", (req, res) => {
 		res.render("index", { title: "Hellouwe", message: "Hello there!" });
 	});
---
+++ src/logic/audioSource/audioSourceTester.js
@@ -0,0 +1,26 @@
+const https = require("https");
+const http = require("http");
+
+module.exports = class AudioSourceTester {
+	async testAudioSource(audioSource) {
+		let getMethod = this.determineGetMethod(audioSource);
+
+		return new Promise((resolve) => {
+			let request = getMethod(audioSource.url, (response) => {
+				resolve(response.statusCode == 200 || response.statusCode == 302);
+			});
+
+			request.on("error", () => {
+				resolve(false);
+			});
+		});
+	}
+
+	determineGetMethod(audioSource) {
+		if (audioSource.url.includes("https")) {
+			return https.get;
+		}
+
+		return http.get;
+	}
+};
