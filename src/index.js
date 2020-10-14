043bfab39099fc8c1511b3b8ad1856f0a9ba3f07
Shaylin Chetty <shaylinc13@gmail.com>
2020-10-14 18:36:22 +0200

    Removed unused imports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 const fs = require("fs");
 const express = require("express");
-const path = require("path");
 const app = express();
-const session = require('express-session');
 
 app.use(express.json());
-app.use(express.static('client/view/'))
+app.use(express.static("client/view/"));
 
 const createRecordingTaskModel = require("./data/recordingTask/createRecordingTaskModel");
 const createAudioSourceModel = require("./data/audioSource/createAudioSourceModel");
