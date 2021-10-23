008bd35161054a3015a2fe51de3a34fc2c7355b0
Shaylin Chetty <shaylin.chetty@protonmail.com>
2021-10-23 13:09:12 +0200

    Improved styling and added project logo

--- src/api/viewRendering/createViewRenderingRoutes.js
+++ src/api/viewRendering/createViewRenderingRoutes.js
@@ -4,6 +4,14 @@ module.exports = (app) => {
     });
 
     app.get("/audioSources", (req, res) => {
-        res.render("audioSources", { title: "Audio Sources" });
+        res.render("audioSources", { title: "Audio Sources", isAuthenticated: !!req.user  });
+    });
+
+    app.get("/clipStorage", (req, res) => {
+        res.render("clipStorage", { title: "ClipStorage", isAuthenticated: !!req.user  });
+    });
+
+    app.get("/recordingTasks", (req, res) => {
+        res.render("recordingTasks", { title: "ClipStorage", isAuthenticated: !!req.user  });
     });
 };
