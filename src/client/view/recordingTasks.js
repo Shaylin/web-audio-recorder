0e439fc1b9e6905bf83a93d8ffb57b2a340181f2
Shaylin Chetty <shaylin.chetty@protonmail.com>
2021-11-06 13:03:47 +0200

    Added recording task creation and deletion

--- src/client/view/recordingTasks.js
+++ src/client/view/recordingTasks.js
@@ -20,8 +20,20 @@ function onCreationConfirmed() {
     const creationModalMinutes = document.getElementById("creation-modal-minutes");
     const creationModalDuration = document.getElementById("creation-modal-duration");
 
-    if (validateHours() || validateAudioSource()) {
-        return;
+    if (validateHours() && validateAudioSource() && validateMinutes() && validateDuration()) {
+        let requestBody = JSON.stringify({
+            audioSourceName: audioSourceText.innerText,
+            hour: parseInt(creationModalHours.value),
+            minute: parseInt(creationModalMinutes.value),
+            duration: parseInt(creationModalDuration.value)
+        });
+
+        fetch("/api/recordingTasks", {method: "POST", body: requestBody, headers: {"Content-Type": "application/json"}})
+            .then(response => response.json())
+            .then(() => {
+                creationModal.classList.remove("is-active");
+                location.reload();
+            });
     }
 }
 
@@ -53,12 +65,70 @@ function validateHours() {
     return false;
 }
 
-function validateAudioSourceText() {
-    const audioSourceText = document.getElementById("audio-source-text");
+function validateMinutes() {
+    const creationModalMinutes = document.getElementById("creation-modal-minutes");
 
+    const minutes = parseInt(creationModalMinutes.value);
+
+    if (minutes > 0 && minutes < 60) {
+        return true;
+    }
+
+    creationModalMinutes.style.backgroundColor = "lightsalmon";
+    creationModalMinutes.oninput = () => {
+        creationModalMinutes.style.backgroundColor = "transparent";
+    };
+
+    return false;
+}
+
+function validateDuration() {
+    const creationModalDuration = document.getElementById("creation-modal-duration");
+
+    const duration = parseInt(creationModalDuration.value);
+
+    if (duration > 0) {
+        return true;
+    }
+
+    creationModalDuration.style.backgroundColor = "lightsalmon";
+    creationModalDuration.oninput = () => {
+        creationModalDuration.style.backgroundColor = "transparent";
+    };
+
+    return false;
 }
 
 function closeCreationModal() {
     const creationModal = document.getElementById("creation-modal");
     creationModal.classList.remove("is-active");
+}
+
+function onRecordingTaskDeleteClicked(taskId) {
+    const deletionModal = document.getElementById("deletion-modal");
+    const deletionModalName = document.getElementById("deletion-modal-name");
+
+    fetch(`/api/recordingTasks/${taskId}`)
+        .then(response => response.json())
+        .then((recordingTask) => {
+            deletionModalName.innerText = `${recordingTask.audioSourceName} ${recordingTask.hour}:${recordingTask.minute} - ${recordingTask.duration}m`;
+            deletionModalName.value = taskId;
+            deletionModal.classList.add("is-active");
+        });
+}
+
+function onDeletionConfirmed() {
+    const deletionModal = document.getElementById("deletion-modal");
+    const deletionModalName = document.getElementById("deletion-modal-name").value;
+
+    fetch(`/api/recordingTasks/${deletionModalName}`, {method: "DELETE"})
+        .then(() => {
+            deletionModal.classList.remove("is-active");
+            location.reload();
+        });
+}
+
+function closeDeletionModal() {
+    const deletionModal = document.getElementById("deletion-modal");
+    deletionModal.classList.remove("is-active");
 }
