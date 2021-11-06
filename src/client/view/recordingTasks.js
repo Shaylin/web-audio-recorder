4ba50dcf78b79cabea21297c32d9ca6fe48c5721
Shaylin Chetty <shaylin.chetty@protonmail.com>
2021-11-06 15:50:08 +0200

    Added recording task editing

--- src/client/view/recordingTasks.js
+++ src/client/view/recordingTasks.js
@@ -5,13 +5,11 @@ function onAddNewRecordingTaskClicked() {
 }
 
 function onAudioSourceSelected(audioSource) {
-    const audioSourceDropDown = document.getElementById("audio-source-dropdown");
     const audioSourceText = document.getElementById("audio-source-text");
     audioSourceText.style.backgroundColor = "transparent";
 
     audioSourceText.innerText = audioSource;
 }
-
 
 function onCreationConfirmed() {
     const creationModal = document.getElementById("creation-modal");
@@ -20,7 +18,7 @@ function onCreationConfirmed() {
     const creationModalMinutes = document.getElementById("creation-modal-minutes");
     const creationModalDuration = document.getElementById("creation-modal-duration");
 
-    if (validateHours() && validateAudioSource() && validateMinutes() && validateDuration()) {
+    if (validateHours(creationModalHours) && validateAudioSource(audioSourceText) && validateMinutes(creationModalMinutes) && validateDuration(creationModalDuration)) {
         let requestBody = JSON.stringify({
             audioSourceName: audioSourceText.innerText,
             hour: parseInt(creationModalHours.value),
@@ -37,66 +35,13 @@ function onCreationConfirmed() {
     }
 }
 
-function validateAudioSource() {
-    const audioSourceText = document.getElementById("audio-source-text");
-
-    if (audioSourceText.innerText.includes("Select Audio Source")) {
-        audioSourceText.style.backgroundColor = "lightsalmon";
+function validateAudioSource(textElement) {
+    if (textElement.innerText.includes("Select Audio Source")) {
+        textElement.style.backgroundColor = "lightsalmon";
         return false;
     }
 
     return true;
-}
-
-function validateHours() {
-    const creationModalHours = document.getElementById("creation-modal-hours");
-
-    const hours = parseInt(creationModalHours.value);
-
-    if (hours > 0 && hours < 24) {
-        return true;
-    }
-
-    creationModalHours.style.backgroundColor = "lightsalmon";
-    creationModalHours.oninput = () => {
-        creationModalHours.style.backgroundColor = "transparent";
-    };
-
-    return false;
-}
-
-function validateMinutes() {
-    const creationModalMinutes = document.getElementById("creation-modal-minutes");
-
-    const minutes = parseInt(creationModalMinutes.value);
-
-    if (minutes > 0 && minutes < 60) {
-        return true;
-    }
-
-    creationModalMinutes.style.backgroundColor = "lightsalmon";
-    creationModalMinutes.oninput = () => {
-        creationModalMinutes.style.backgroundColor = "transparent";
-    };
-
-    return false;
-}
-
-function validateDuration() {
-    const creationModalDuration = document.getElementById("creation-modal-duration");
-
-    const duration = parseInt(creationModalDuration.value);
-
-    if (duration > 0) {
-        return true;
-    }
-
-    creationModalDuration.style.backgroundColor = "lightsalmon";
-    creationModalDuration.oninput = () => {
-        creationModalDuration.style.backgroundColor = "transparent";
-    };
-
-    return false;
 }
 
 function closeCreationModal() {
@@ -131,4 +76,108 @@ function onDeletionConfirmed() {
 function closeDeletionModal() {
     const deletionModal = document.getElementById("deletion-modal");
     deletionModal.classList.remove("is-active");
+}
+
+function onRecordingTaskEditClicked(taskId) {
+    const editModal = document.getElementById("edit-modal");
+    editModal.classList.add("is-active");
+
+    fetch(`/api/recordingTasks/${taskId}`)
+        .then(response => response.json())
+        .then((recordingTask) => {
+            const audioSourceText = document.getElementById("edit-source-text");
+            const editModalHours = document.getElementById("edit-modal-hours");
+            const editModalMinutes = document.getElementById("edit-modal-minutes");
+            const editModalDuration = document.getElementById("edit-modal-duration");
+            editModal.value = taskId;
+
+            audioSourceText.innerText = recordingTask.audioSourceName;
+            editModalHours.value = recordingTask.hour;
+            editModalMinutes.value = recordingTask.minute;
+            editModalDuration.value = recordingTask.duration;
+
+            editModal.classList.add("is-active");
+        });
+}
+
+function onEditAudioSourceSelected(audioSource) {
+    const audioSourceText = document.getElementById("edit-source-text");
+    audioSourceText.style.backgroundColor = "transparent";
+
+    audioSourceText.innerText = audioSource;
+}
+
+function closeEditModal() {
+    const editModal = document.getElementById("edit-modal");
+    editModal.classList.remove("is-active");
+}
+
+function onEditModalConfirmed() {
+    const editModal = document.getElementById("edit-modal");
+    const audioSourceText = document.getElementById("edit-source-text");
+    const editModalHours = document.getElementById("edit-modal-hours");
+    const editModalMinutes = document.getElementById("edit-modal-minutes");
+    const editModalDuration = document.getElementById("edit-modal-duration");
+
+    if (validateHours(editModalHours) && validateAudioSource(audioSourceText) && validateMinutes(editModalMinutes) && validateDuration(editModalDuration)) {
+        const requestBody = JSON.stringify({
+            id: editModal.value,
+            audioSourceName: audioSourceText.innerText,
+            hour: parseInt(editModalHours.value),
+            minute: parseInt(editModalMinutes.value),
+            duration: parseInt(editModalDuration.value)
+        });
+
+        fetch("/api/recordingTasks", {method: "PUT", body: requestBody, headers: {"Content-Type": "application/json"}})
+            .then(response => response.json())
+            .then(() => {
+                editModal.classList.remove("is-active");
+                location.reload();
+            });
+    }
+}
+
+function validateHours(hoursInput) {
+    const hours = parseInt(hoursInput.value);
+
+    if (hours >= 0 && hours < 24) {
+        return true;
+    }
+
+    hoursInput.style.backgroundColor = "lightsalmon";
+    hoursInput.oninput = () => {
+        hoursInput.style.backgroundColor = "transparent";
+    };
+
+    return false;
+}
+
+function validateMinutes(inputElement) {
+    const minutes = parseInt(inputElement.value);
+
+    if (minutes >= 0 && minutes < 60) {
+        return true;
+    }
+
+    inputElement.style.backgroundColor = "lightsalmon";
+    inputElement.oninput = () => {
+        inputElement.style.backgroundColor = "transparent";
+    };
+
+    return false;
+}
+
+function validateDuration(inputElement) {
+    const duration = parseInt(inputElement.value);
+
+    if (duration > 0) {
+        return true;
+    }
+
+    inputElement.style.backgroundColor = "lightsalmon";
+    inputElement.oninput = () => {
+        inputElement.style.backgroundColor = "transparent";
+    };
+
+    return false;
 }
